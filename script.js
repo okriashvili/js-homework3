b161848b5cd349896489e5481c0d6e85d0289ab0
mahoo <146084234+ADM-olivier@users.noreply.github.com>
2025-03-28 17:26:30 +0400

    finished homework javascript homework 3

---
+++ script.js
@@ -0,0 +1,20 @@
+// 2,1 დავალება
+var surName = "Okriashvili"
+console.log(surName.length)
+
+// 2,2 დვალება
+var x = "ana"
+console.log(x.toUpperCase())
+
+var x = "gia"
+console.log(x.toLowerCase())
+
+
+// დავალება 2,3
+var capital = "თბილისი"
+console.log(capital.substring(0, 5))
+
+// დავალება 2,4
+var string = 'Hello world, this is string';
+string = string.replace("string", "me")
+console.log(string.replace("world", "Tbilisi"))
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+// დავალება 2,4
+let string = 'Hello world, this is string';
+console.log(string.replace(2, "Tbilisi"))
